532a321007b0c2c4e098bc5bc5db197ca94516ee
Pierre <pierreb.montagne@gmail.com>
2022-04-09 16:30:33 -0400

    Check for username and email already in database and give error message

--- register.php
+++ register.php
@@ -1,4 +1,7 @@
 <?php
+	if (session_status() === PHP_SESSION_NONE) {
+		session_start();
+	}
 	$server = "localhost";
 	$username = "root";
 	$password = "";
@@ -20,9 +23,31 @@
 	$credit_info = $_POST['credit_info'];
 	$credit_cvv = $_POST['credit_cvv'];
 
-	if(isset($_POST['submit'])){
-		if(!empty($email) && !empty($name) && !empty($username) && !empty($password) && !empty($address) && !empty($state) && !empty($zip) && !empty($credit_info) && !empty($credit_cvv)) {
+	//Username Query
+	$stmt = $conn-> prepare("select * from account where username = ?");
+	$stmt->bind_param("s", $username);
+	$stmt->execute();
+	$stmt_result = $stmt->get_result();
 
+	//Email query
+	$stmt1 = $conn-> prepare("select * from account where email = ?");
+	$stmt1->bind_param("s", $email);
+	$stmt1->execute();
+	$stmt_result1 = $stmt1->get_result();
+
+	//Check for username or email taken
+	if($stmt_result->num_rows > 0 || $stmt_result1->num_rows > 0){
+		if ($stmt_result->num_rows > 0){
+			$_SESSION['errUsernameMsg'] = "Username Taken";
+		}
+		if ($stmt_result1->num_rows > 0){
+        	$_SESSION['errEmailMsg'] = "Email Taken";
+		}
+		include('signup.html');
+	}
+	else if(isset($_POST['submit'])){
+		if(!empty($email) && !empty($name) && !empty($username) && !empty($password) && !empty($address) && !empty($state) && !empty($zip) && !empty($credit_info) && !empty($credit_cvv)) {
+			
 			$query = "insert into account (email, name, username, password, address,zip,state,credit_info,credit_cvv) values ('$email','$name','$username','$password','$address','$state','$zip','$credit_info','$credit_cvv')";
 			
 			mysqli_query($conn,$query);
